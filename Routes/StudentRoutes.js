9b30f34a0e04cc46e0073556f0cffe77b0ecc615
sudeep <sudeepasmith8@gmail.com>
2019-01-08 23:46:08 +0530

    Angular-Mongo

--- Routes/StudentRoutes.js
+++ Routes/StudentRoutes.js
@@ -14,7 +14,7 @@ MongoClient.connect(url, function(err, client){
         res.send(err);
     }else{
 
-        const collectionName = "StudentColl";
+        const collectionName = "studentcollections";
 
         let collection = client.db(database).collection(collectionName);
 
@@ -29,118 +29,165 @@ MongoClient.connect(url, function(err, client){
     }
 });*/
 
-router.get("/",function(req, res){
+router.get("/", function(req, res) {
+  MongoClient.connect(
+    url,
+    function(err, client) {
+      if (err) {
+        //throw err;
+        res.send(err);
+      } else {
+        const collectionName = "studentcollections";
 
-    MongoClient.connect(url, function(err, client){
-        if(err){
-            
-            //throw err;
-            res.send(err);
-        }else{
-    
-            const collectionName = "StudentColl";
-    
-            let collection = client.db(database).collection(collectionName);
-    
-            collection.find({}, function(err, data){
-                //console.log(err, data.toArray());
-                if(err){
-                    res.send(err);
-                }else{
-                    data.toArray().then(function(result){
-                        console.log(result);
-                        res.send(result);
-                    }).catch(function(err){
-                        res.send(err);
-                    });
-                }
-                client.close();
-            });
-        }
-    });
-    
+        let collection = client.db(database).collection(collectionName);
+
+        collection.find(
+          {},
+          //{ projection: { ID: 1, Name: 1, createdAt: 1, updatedAt: 1 } },
+          function(err, data) {
+            // console.log(err, data.toArray());
+            if (err) {
+              res.send(err);
+            } else {
+              data
+                .toArray()
+                .then(function(result) {
+                  //res.setHeader("Content-Type", "application/json");
+                  //console.log(result);
+                  setTimeout(() => {
+                    res.send(result).status(200);
+                  }, 2000);
+                })
+                .catch(function(err) {
+                  res.send(err);
+                });
+            }
+            client.close();
+          }
+        ); //
+      }
+    }
+  );
 });
 
-router.post("/",function(req, res){
+router.get("/:id", function(req, res) {
+  MongoClient.connect(
+    url,
+    function(err, client) {
+      if (err) {
+        //throw err;
+        res.send(err);
+      } else {
+        const collectionName = "studentcollections";
 
-    MongoClient.connect(url, function(err, client){
-        if(err){
-            
-            //throw err;
+        let collection = client.db(database).collection(collectionName);
+
+        collection.find({ ID: req.params.id }, function(err, data) {
+          //console.log(err, data.toArray());
+          if (err) {
             res.send(err);
-        }else{
-    
-            const collectionName = "StudentColl";
-    
-            let collection = client.db(database).collection(collectionName);
-    
-            collection.insertOne(req.body, function(err, data){
-                if(err){
-                    res.send(err);
-                }else{
-                    //console.log(data);
-                    res.send(data);
-                }
-                client.close();
-            });
-        }
-    });
-    
+          } else {
+            data
+              .toArray()
+              .then(function(result) {
+                //console.log(result);
+                res.status(200).send(result);
+              })
+              .catch(function(err) {
+                res.send(err);
+              });
+          }
+          client.close();
+        });
+      }
+    }
+  );
 });
 
-router.delete("/:id",function(req, res){
-    MongoClient.connect(url, function(err, client){
-        if(err){
-            
-            //throw err;
+router.post("/", function(req, res) {
+  MongoClient.connect(
+    url,
+    function(err, client) {
+      if (err) {
+        //throw err;
+        res.send(err);
+      } else {
+        const collectionName = "studentcollections";
+
+        let collection = client.db(database).collection(collectionName);
+
+        collection.insertOne(req.body, function(err, data) {
+          if (err) {
             res.send(err);
-        }else{
-    
-            const collectionName = "StudentColl";
-    
-            let collection = client.db(database).collection(collectionName);
-    
-            collection.deleteOne({id:req.param}, function(err, data){
-                if(err){
-                    res.send(err);
-                }else{
-                    //console.log(data);
-                    res.send(data);
-                }
-                client.close();
-            });
-        }
-    });
+          } else {
+            //console.log(data);
+            res.send(data);
+          }
+          client.close();
+        });
+      }
+    }
+  );
 });
 
-router.put("/:id",function(req, res){
+router.delete("/:id", function(req, res) {
+  MongoClient.connect(
+    url,
+    function(err, client) {
+      if (err) {
+        //throw err;
+        res.send(err);
+      } else {
+        const collectionName = "studentcollections";
 
-    MongoClient.connect(url, function(err, client){
-        if(err){
-            
-            //throw err;
+        let collection = client.db(database).collection(collectionName);
+
+        collection.deleteOne({ id: req.param }, function(err, data) {
+          if (err) {
             res.send(err);
-        }else{
-    
-            const collectionName = "StudentColl";
-    
-            let collection = client.db(database).collection(collectionName);
-    
-            collection.updateOne({"_id":req.param},{$set:req.body}, function(err, data){
-                if(err){
-                    res.send(err);
-                }else{
-                    //console.log(data);
-                    res.send(data);
-                }
-                client.close();
-            });
-        }
-    });
+          } else {
+            //console.log(data);
+            res.send(data);
+          }
+          client.close();
+        });
+      }
+    }
+  );
 });
 
-router.all(function(req, res){
-    console.log(" > "+req.body);
+router.put("/:id", function(req, res) {
+  MongoClient.connect(
+    url,
+    function(err, client) {
+      if (err) {
+        //throw err;
+        res.send(err);
+      } else {
+        const collectionName = "studentcollections";
+
+        let collection = client.db(database).collection(collectionName);
+        //console.log(req.body);
+        collection.updateOne(
+          { _id: req.param.id },
+          { $set: req.body },
+          function(err, data) {
+            if (err) {
+              res.send(err);
+            } else {
+              //console.log(data);
+              res.send(data);
+            }
+            client.close();
+          }
+        );
+      }
+    }
+  );
+});
+
+router.all(function(req, res) {
+  console.log(" > " + req.body);
 });
 
 module.exports = router;
--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require("express");
 var path = require("path");
 var bodyParser = require("body-parser");
 var cookieParser = require("cookie-parser");
+var cors = require("cors");
 
 /* Routes */
 var HomeRoutes = require("./Routes/HomeRoutes");
@@ -11,7 +12,7 @@ var StudentRoutes = require("./Routes/StudentRoutes");
 
 var app = express();
 
-app.use(express.static(path.join(__dirname,"/libs")));
+app.use(express.static(path.join(__dirname, "/libs")));
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -21,25 +22,48 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 
 /* This is will be executed for Every Request */
-app.use(function(req, res, next){
-    //console.log(req.cookies);
-    console.log("Request -> "+ req.originalUrl +" Received on -> "+new Date());
-    next();
+app.use(function(req, res, next) {
+  //console.log(req.cookies);
+  console.log(
+    "Request -> " + req.originalUrl + " Received on -> " + new Date()
+  );
+  //console.log(req.headers);
+  next();
 });
 
-app.use(function(err, req, res, next){
+const corsOptions = {
+  origin: "http://localhost:4200",
+  allowedHeaders: "Content-Type, Accept, Authorization"
+};
+app.options("*", cors());
 
-    console.log(err.stack);
-    res.status(500).send("Something went wrong");
+///* Verify Later */
+//app.use(function(req, res, next) {
+//  res.header("Access-Control-Allow-Origin", "*");
+//  res.header(
+//    "Access-Control-Allow-Headers",
+//    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+//  );
+//	/*
+//  if(!req.headers.authorization)
+//  {
+//	  res.status(401);
+//	  res.send(null);
+//	return false;
+//  }*/
+//  next();
+//});
+
+app.use(function(err, req, res, next) {
+  console.log(err.stack);
+  res.status(500).send("Something went wrong");
 });
 
-app.use('/',HomeRoutes);
-app.use('/Products',ProductRoutes);
-app.use('/Students',StudentRoutes);
+app.use("/", HomeRoutes);
+app.use("/Products", ProductRoutes);
+app.use("/Students", StudentRoutes);
 
 const port = 6800;
-app.listen(port,function(){
-
-    console.log("Server has been initialized on port -> "+port);
+app.listen(port, function() {
+  console.log("Server has been initialized on port -> " + port);
 });
-
